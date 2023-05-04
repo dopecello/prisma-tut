068236fd459fb029757ea49f3f8a9e09967e357f
dopecello <williamlocke.cello@gmail.com>
2023-05-04 17:42:08 -0400

    added first data ever with prisma :)

--- script.ts
+++ script.ts
@@ -1,2 +1,21 @@
-import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+
+async function main() {
+  const user = await prisma.user.create({
+    data: {
+      email: 'crazydev@gmail.com',
+      name: 'William',
+      password: 'secretsauce123',
+    },
+  });
+  console.log(user);
+}
+
+main()
+  .catch((e) => {
+    console.error(e.message);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
